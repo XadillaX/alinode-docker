27465bfd8ed01dc74c01021ff31f086f53fb92f9
Jackson Tian <shyvo1987@gmail.com>
2017-05-09 17:29:09 +0800

    rename agentx.js to agentx.default.js

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 source $HOME/.bashrc
 
 if [ -f $HOME/alinode-app.json ]; then
-    ENABLE_NODE_LOG=NO node -p 'Object.assign(require("$HOME/agentx.js"), require("$HOME/alinode-app.json"))' > $HOME/agentx.json
+    ENABLE_NODE_LOG=NO node -p 'Object.assign(require("$HOME/agentx.default.js"), require("$HOME/alinode-app.json"))' > $HOME/agentx.json
     ENABLE_NODE_LOG=NO agentx $HOME/agentx.json &
 fi
 
